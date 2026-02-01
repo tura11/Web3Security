7d9faadf73def6f80b79bc3bc4975f7550673f8b
Tymek <tymek_uramek@wp.pl>
2026-02-01 16:47:50 +0100

    Correct natspec

--- audits/passwordstore/src/PasswordStore.sol
+++ audits/passwordstore/src/PasswordStore.sol
@@ -37,7 +37,7 @@ contract PasswordStore {
      * @param newPassword The new password to set.
      */
 
-     //@audit-medium, missing param like newPassword
+     //@audit-low, missing param like newPassword
     function getPassword() external view returns (string memory) {
         if (msg.sender != s_owner) {
             revert PasswordStore__NotOwner();
