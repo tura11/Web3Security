75fc6cf7ccfc14e346b41bc3ad7c4d081ec26ab5
TymekU <tura11@users.noreply.github.com>
2026-02-07 20:19:34 +0100

    audit-high, reentracy

--- audits/puppy-raffle/src/PuppyRaffle.sol
+++ audits/puppy-raffle/src/PuppyRaffle.sol
@@ -98,7 +98,7 @@ contract PuppyRaffle is ERC721, Ownable {
         require(playerAddress == msg.sender, "PuppyRaffle: Only the player can refund");
         require(playerAddress != address(0), "PuppyRaffle: Player already refunded, or is not active");
 
-        payable(msg.sender).sendValue(entranceFee);
+        payable(msg.sender).sendValue(entranceFee); // audit-high reentrancy possibility, check CEI pattern!!!
 
         players[playerIndex] = address(0);
         emit RaffleRefunded(playerAddress);
