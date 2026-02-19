ca57e7d5710875dbc1f37e525fc5eb24b7937933
Tymek <tymek_uramek@wp.pl>
2026-02-19 21:12:51 +0100

    Invarinat

---
+++ audits/5-t-swap-audit/test/invariant/Invariant.t.sol
@@ -0,0 +1,34 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.20;
+
+import {Test} from "forge-std/Test.sol";
+import {StdInvariant} from "forge-std/StdInvariant.sol";
+import {ERC20Mock} from "../mocks/ERC20Mock.sol";
+import {PoolFactory} from "../../src/PoolFactory.sol";
+import {TSwapPool} from "../../src/TSwapPool.sol";
+
+contract Invariant is StdInvariant, Test {
+    ERC20Mock poolToken;
+    ERC20Mock weth;
+
+    PoolFactory factory;
+    TSwapPool pool;
+
+    int256 constant STARTING_X = 100e18; //erc20 pool token
+    int256 constant STARTING_Y = 50e18; // weth
+
+    function setUp(){
+        weth = new ERC20Mock();
+        poolToken = new ERC20Mock();
+        factory = new PoolFactory(address(weth));
+        pool = TSwapPool(factory.createPool(address(poolToken)));
+
+        poolToken.mint(address(this), uint256(STARTING_X));
+        weth.mint(address(this), uint256(STARTING_Y));
+   
+        poolToken.approve(address(pool), type(uint256).max);
+        weth.approve(address(pool), type(uint256).max);
+
+        pool.deposit(uint256(STARTING_Y), uint256(STARTING_Y), uint256(STARTING_X), block.timestamp);
+    }
+}
